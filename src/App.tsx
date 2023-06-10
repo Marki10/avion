b558d3208b46fac31d0752cabb5e54f89efa50fd
Mark Darabos <Marki10@users.noreply.github.com>
2023-06-10 11:17:45 +0100

    Create Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Pages/Home";
+
 import "./App.scss";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header></Header>
+      <Home></Home>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faMagnifyingGlass, faUser } from "@fortawesome/free-solid-svg-icons";
+
+import "./header.scss";
+
 const Header = () => {
+  const menu = [
+    { id: 1, link: "", name: "Plant pots" },
+    { id: 2, link: "", name: "Ceramics" },
+    { id: 3, link: "", name: "Tables" },
+    { id: 4, link: "", name: "Chairs" },
+    { id: 5, link: "", name: "Crockery" },
+    { id: 6, link: "", name: "Tableware" },
+    { id: 7, link: "", name: "Cutlery" },
+  ];
+
   return (
     <header className="header">
       <div className="header-top">
-        <div className="header-top__search"></div>
-        <div className="header-top__logo"></div>
-        <div className="header-top__profile"></div>
+        <div className="header-top__search">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </div>
+        <div className="header-top__logo">Avion</div>
+        <div className="header-top__profile">
+          <FontAwesomeIcon icon={faUser} />
+        </div>
       </div>
       <ul className="main-menu">
-        <li className="main-menu__item"></li>
+        {menu.map((menuItem: any) => {
+          return <li className="main-menu__item">{menuItem.name}</li>;
+        })}
       </ul>
     </header>
   );
